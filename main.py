f5f9ee9db32e923384efa73d8062e17824c86cfc
Olat1337 <Olat.kudrevich@gmail.com>
2026-05-29 21:57:04 +0200

    feat: add PDF loading and text splitting with LangChain

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 def main():
-    print()
+    loader = PyPDFLoader("data/rules.pdf")
+    pages = loader.load()
+    print(len(pages))
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_documents(pages)
+    print(len(chunks))
+    print(chunks[0].page_content)
 
 if __name__ == '__main__':
     main()
