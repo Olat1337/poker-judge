9bedfee761e83d79929b0ed4fa9790b9d95a94ab
Olat1337 <Olat.kudrevich@gmail.com>
2026-05-30 18:56:34 +0200

    refactor: restructure RAG pipeline

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
-def main():
-    loader = PyPDFLoader("data/rules.pdf")
+PDF_FILE_PATH = "data/rules.pdf"
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
+
+def load_split_pdf(file_path):
+    print("LOADING AND SPLITTING PDF")
+    loader = PyPDFLoader(file_path)
     pages = loader.load()
-    print(len(pages))
+    print("PAGES IN PDF: "+str(len(pages)))
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -14,15 +18,27 @@ def main():
     )
 
     chunks = text_splitter.split_documents(pages)
-    print(len(chunks))
+    print("AMOUNT OF CHUNKS: "+ str(len(chunks)))
+    print("CHECK FIRST CHUNK:")
     print(chunks[0].page_content)
+    return chunks
 
-    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+def create_vector_db(chunks):
+    print("CREATING VECTOR DB")
+    embedding_model = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
     db = Chroma.from_documents(chunks, embedding_model)
 
-    query = "What happens if I bet a single oversized chip?"
+    return db
+def main():
+    chunks = load_split_pdf(PDF_FILE_PATH)
+    db = create_vector_db(chunks)
 
+    #"TEST SEARCH"
+    query = "What happens if I bet a single oversized chip?"
+    print(f"SEARCHING ANSWER TO THE QUESTION: {query}")
     docs = db.similarity_search(query)
+    print("FOUND RULE")
     print(docs[0].page_content)
+
 if __name__ == '__main__':
     main()
