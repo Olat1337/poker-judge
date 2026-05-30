4380d28d9b8a93d8cf5b9297c242e122983627b4
Olat1337 <Olat.kudrevich@gmail.com>
2026-05-31 00:38:39 +0200

    feat: complete RAG pipeline and implement ask_judge function

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.llms import Ollama
+from langchain_core.prompts import PromptTemplate
 
 PDF_FILE_PATH = "data/rules.pdf"
 EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 LLM_MODEL = "llama3"
 
+def ask_judge(query, db, llm):
+    docs = db.similarity_search(query, k=3)
+    context = "\n\n".join([doc.page_content for doc in docs])
+    template_text = """You are a strict poker tournament judge. Answer the player's question using ONLY the provided rules. If the answer is not in the rules, say "I don't know".
+
+    Rules:
+    {context}
+
+    Question: {question}
+
+    Answer:"""
+
+    template = PromptTemplate.from_template(template_text)
+    final_prompt = template.format(context=context, question = query)
+    response = llm.invoke(final_prompt)
+
+    return response
+
 def setup_llm(llm_model):
     llm = Ollama(model=llm_model)
+
     return llm
 
 def load_split_pdf(file_path):
@@ -25,8 +45,7 @@ def load_split_pdf(file_path):
 
     chunks = text_splitter.split_documents(pages)
     print("AMOUNT OF CHUNKS: "+ str(len(chunks)))
-    print("CHECK FIRST CHUNK:")
-    print(chunks[0].page_content)
+
     return chunks
 
 def create_vector_db(chunks, embedding_model_name):
@@ -35,22 +54,17 @@ def create_vector_db(chunks, embedding_model_name):
     db = Chroma.from_documents(chunks, embedding_model)
 
     return db
+
 def main():
     chunks = load_split_pdf(PDF_FILE_PATH)
     db = create_vector_db(chunks, EMBEDDING_MODEL_NAME)
-
-    #"TEST SEARCH"
-    query = "What happens if I bet a single oversized chip?"
-    print(f"SEARCHING ANSWER TO THE QUESTION: {query}")
-    docs = db.similarity_search(query)
-    print("FOUND RULE")
-    print(docs[0].page_content)
-
     llm = setup_llm(LLM_MODEL)
 
-    print("SENDING TEST REQUEST TO LLM")
-    response = llm.invoke("Hello! Are you ready to act as a poker judge?")
+    query = "What happens if I bet a single oversized chip?"
+    response = ask_judge(query, db, llm)
+
+    print("JUDGE'S VERDICT: ")
     print(response)
-    
+
 if __name__ == '__main__':
     main()
