b59a089f3ddd1f0892c5995e4de9986c0fba9d68
Olat1337 <Olat.kudrevich@gmail.com>
2026-05-30 18:49:09 +0200

    feat: add ChromaDB and semantic search with embeddings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def main():
     loader = PyPDFLoader("data/rules.pdf")
@@ -15,5 +17,12 @@ def main():
     print(len(chunks))
     print(chunks[0].page_content)
 
+    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma.from_documents(chunks, embedding_model)
+
+    query = "What happens if I bet a single oversized chip?"
+
+    docs = db.similarity_search(query)
+    print(docs[0].page_content)
 if __name__ == '__main__':
     main()
